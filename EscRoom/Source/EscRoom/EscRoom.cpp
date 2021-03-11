38f5a4ee81fc97a7a9046d2609f83c94bcb14fbc
AnimeAdam <Adam.Szlamp@gmail.com>
2021-03-11 19:16:24 +0000

    Unreal Game added

---
+++ EscRoom/Source/EscRoom/EscRoom.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EscRoom.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EscRoom, "EscRoom" );
